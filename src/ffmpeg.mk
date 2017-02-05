5f11f6826c8dfb1ebc7441442dcc8562561f9176
Zach Bacon <zachbacon@budgiemedia.rocks>
2017-02-04 21:50:56 -0500

    Reverted ffmpeg for now

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -1,21 +1,17 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := ffmpeg
+$(PKG)_WEBSITE  := http://www.ffmpeg.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.2
-$(PKG)_CHECKSUM := 3f01bd1fe1a17a277f8c84869e5d9192b4b978cb660872aa2b54c3cc8a2fedfc
+$(PKG)_VERSION  := 3.2.1
+$(PKG)_CHECKSUM := 72abc55bea5ff5397ac82320fa5c4843a05f527d0d7912d66784c92fdfbd12fb
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.ffmpeg.org/releases/$($(PKG)_FILE)
 $(PKG)_URL_2    := http://launchpad.net/ffmpeg/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_VERSION)/+download/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc bzip2 gnutls lame libass libbluray libcaca \
-                   libvpx opencore-amr opus sdl2 speex theora vidstab \
-                   vo-amrwbenc vorbis x264 xvidcore yasm zlib
-
-# DO NOT ADD fdk-aac OR openssl SUPPORT.
-# Although they are free softwares, their licenses are not compatible with
-# the GPL, and we'd like to enable GPL in our default ffmpeg build.
-# See docs/index.html#potential-legal-issues
+$(PKG)_DEPS     := gcc bzip2 gnutls lame libass libbluray libbs2b libcaca \
+                   libvpx opencore-amr opus sdl speex theora vidstab \
+                   vo-amrwbenc vorbis x264 xvidcore yasm zlib fdk-aac
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://ffmpeg.org/releases/' | \
@@ -43,12 +39,15 @@ define $(PKG)_BUILD
         --disable-doc \
         --enable-avresample \
         --enable-gpl \
+        --enable-nonfree \
         --enable-version3 \
         --extra-libs='-mconsole' \
         --enable-avisynth \
         --enable-gnutls \
         --enable-libass \
         --enable-libbluray \
+        --enable-libfdk-aac \
+        --enable-libbs2b \
         --enable-libcaca \
         --enable-libmp3lame \
         --enable-libopencore-amrnb \
@@ -58,8 +57,6 @@ define $(PKG)_BUILD
         --enable-libtheora \
         --enable-libvidstab \
         --enable-libvo-amrwbenc \
-        --enable-sdl2 \
-        --enable-lto \
         --enable-libvorbis \
         --enable-libvpx \
         --enable-libx264 \
